33d3860193f50d4648fe88be258a63bbcf6e0fb6
JoAndrie <89676842+JoAndrie@users.noreply.github.com>
2023-03-24 16:45:31 +0800

    added Woocommerce order model and admin

--- PaymentGateway/admin.py
+++ PaymentGateway/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from PaymentGateway.models import WoocommerceOrder
 # Register your models here.
+
+class WoocommerceOrderAdmin(admin.ModelAdmin):
+    list_display = ("order_id", "customer_name", "status", "created_at", "updated_at", "total")
+    list_filter = ("created_at", "status")
+    search_fields = ("order_id", "customer_name", "status")
+
+    def has_add_permission(self, request, obj=None):
+        return False
+    
+
+admin.site.register(WoocommerceOrder, WoocommerceOrderAdmin)
--- PaymentGateway/models.py
+++ PaymentGateway/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class WoocommerceOrder(models.Model):
+    order_id = models.IntegerField(primary_key=True)
+    customer_name = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    
