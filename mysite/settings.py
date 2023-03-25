9af41b3c9a939b0dad15ead0ced27e687851d931
JoAndrie <89676842+JoAndrie@users.noreply.github.com>
2023-03-25 16:46:33 +0800

    added corsheaders

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PaymentGateway.apps.PaymentgatewayConfig',
+    'corsheaders',
+
+
+    
+
 ]
 
 MIDDLEWARE = [
@@ -47,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'mysite.urls'
