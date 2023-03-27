446a1c79af7273df04df2fcf09a0112aecc125a4
JoAndrie <89676842+JoAndrie@users.noreply.github.com>
2023-03-27 10:44:06 +0800

    added views for list of orders

--- PaymentGateway/views.py
+++ PaymentGateway/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import WoocommerceOrder
 # Create your views here.
+
+def order_list(request):
+    orders = WoocommerceOrder.objects.all()
+    data = [{
+        'order_id': WoocommerceOrder.order_id,
+        'customer_name': WoocommerceOrder.customer_name,
+        'status': WoocommerceOrder.status,
+        'total': WoocommerceOrder.total,
+        'created_at': WoocommerceOrder.created_at,
+        'updated_at': WoocommerceOrder.updated_at
+    } for WoocommerceOrder in orders]
+
+    return JsonResponse(data, safe = False)
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from PaymentGateway.views import order_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('paymentgateway/orders/', order_list, name = 'order_list')
 ]
