fdacdd1b4f71baf515313405fae08ce3879be551
KerrMicko <mixy7777@gmail.com>
2023-03-28 10:51:35 +0800

    modified user and woocommerceorder models

--- PaymentGateway/models.py
+++ PaymentGateway/models.py
@@ -8,6 +8,9 @@ class User(models.Model):
     username = models.CharField(max_length=255, null=False)
     password = models.CharField(max_length=100, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
 
     def save(self, *args, **kwargs):
         self.password = make_password(self.password)
@@ -21,6 +24,9 @@ class WoocommerceOrder(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.username
 
 class KeyAndSecret(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -29,3 +35,4 @@ class KeyAndSecret(models.Model):
     consumer_secret = models.CharField(max_length=255, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
