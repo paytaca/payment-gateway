3dd6585aec18fcbcf73111e7858e5012d67d4ea2
JoAndrie <89676842+JoAndrie@users.noreply.github.com>
2023-03-29 13:26:52 +0800

    modified get_order command

--- PaymentGateway/management/commands/get_order.py
+++ PaymentGateway/management/commands/get_order.py
@@ -1,24 +1,25 @@
 from django.core.management.base import BaseCommand
 from woocommerce import API
-from PaymentGateway.models import WoocommerceOrder, KeyAndSecret
+from PaymentGateway.models import Order, Storefront
 
 class Command(BaseCommand):
-    help = 'Get WooCommerce orders and store them in the database'
+    help = 'Get orders and store them in the database'
 
     def handle(self, *args, **options):
-        for key_and_secret in KeyAndSecret.objects.all():
+        for store in Storefront.objects.filter(store_type='woocommerce'):
             wcapi = API(
-                url=key_and_secret.store_url, #Store url
-                consumer_key=key_and_secret.consumer_key, #consumer key from woocommerce setting
-                consumer_secret=key_and_secret.consumer_secret, #consumer secret from woocommerce setting
+                url=store.store_url, #Store url
+                consumer_key=store.key, #consumer key from woocommerce setting
+                consumer_secret=store.secret, #consumer secret from woocommerce setting
                 version="wc/v3",
                 verify_ssl = False,
             )
             orders = wcapi.get("orders").json()
             for order in orders:
-                WoocommerceOrder.objects.update_or_create(
+                Order.objects.update_or_create(
                     order_id=order['id'],
-                    username=key_and_secret.username,
+                    store=store.store_type,
+                    user=store.user,
                     defaults={
                         'customer_name': order['billing']['first_name'] + ' ' + order['billing']['last_name'],
                         'total': order['total'],
