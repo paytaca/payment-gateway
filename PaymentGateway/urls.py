a52df332608bbf38291ecdc74e1a11f796007ffd
JoAndrie <89676842+JoAndrie@users.noreply.github.com>
2023-04-18 13:00:26 +0800

    Added GetOrderAPIView to retrieve order details and save in order model

--- PaymentGateway/urls.py
+++ PaymentGateway/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProcessOrderAPIView
+from .views import ProcessOrderAPIView, GetOrderAPIView
 
 urlpatterns = [
     path('process-order/', ProcessOrderAPIView.as_view(), name='process_order'),
+    path('get-order/', GetOrderAPIView.as_view(), name='get-order'),
+
 ]
--- PaymentGateway/views.py
+++ PaymentGateway/views.py
@@ -1,9 +1,56 @@
 import base64
 import requests
+from datetime import datetime
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from woocommerce import API
 from django.shortcuts import get_object_or_404
 from .models import Order, Storefront
+
+class GetOrderAPIView(APIView):
+    def post(self, request, format=None):
+        # extract order details from the request
+        order_id = request.data.get('order_id')
+        store_url = request.data.get('store')
+
+        # check if the store_url exists in Storefront model
+        try:
+            storefront = Storefront.objects.get(store_url=store_url)
+        except Storefront.DoesNotExist:
+            return Response({'message': 'Invalid store URL'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # create the order only if the storefront exists using WooCommerceAPI
+        wcapi = API(
+            url=storefront.store_url, #Store url
+            consumer_key=storefront.key, #consumer key from woocommerce setting
+            consumer_secret=storefront.secret, #consumer secret from woocommerce setting
+            version="wc/v3",
+            verify_ssl = False,
+        )
+
+        # get the order using the order_id and the WooCommerceAPI
+        response = wcapi.get(f"orders/{order_id}")
+        if response.status_code != 200:
+            return Response({'message': 'Invalid order ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+        order_data = response.json()
+
+        # save the order in the Order model
+        order = Order(
+            user=storefront.user,
+            store=storefront.store_url,
+            order_id=order_data['id'],
+            customer_name=order_data['billing']['first_name'] + ' ' + order_data['billing']['last_name'],
+            status=order_data['status'],
+            total=float(order_data['total']),
+            payment_method=order_data['payment_method'],
+            created_at=order_data['date_created_gmt'],
+            updated_at=order_data['date_modified_gmt']
+        )
+        order.save()
+
+        return Response({'message': 'Order saved successfully'}, status=status.HTTP_201_CREATED)
 
 class ProcessOrderAPIView(APIView):
     def post(self, request):
@@ -17,6 +64,7 @@ class ProcessOrderAPIView(APIView):
         try:
             # Update the status of the order
             order.status = 'completed'
+            order.updated_at = datetime.now()
             order.save()
 
             # Mark the order as completed in WooCommerce using the REST API
